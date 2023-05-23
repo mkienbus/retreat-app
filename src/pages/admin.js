fd1f3b1805f74ffa4102a3b74e20c63881a4f8a6
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-05-23 09:54:27 -0600

    added LoginForm to Admin page - untested

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
+import LoginForm from './components/LoginForm'
+import styles from '@/styles/admin.module.css'
 
 export default function Admin(){
 
     return(
         <>
-            <Link href="/">Home</Link>
-            <h2>Administrator portal</h2>
-            <footer>
-                <Link href="/">Logout</Link>
-            </footer>
+            <div className = {styles.adminLogin}>
+                <Link href="/">Home</Link>
+                <h2>Administrator portal</h2>
+                <LoginForm />
+                <footer>
+                    <Link href="/">Logout</Link>
+                </footer>
+            </div>
         </>
     )
 }
