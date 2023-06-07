5dce3276bddcb4fd7349d6186ec79fb9cfa99aec
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-06-06 22:07:07 -0600

    added AdminAccount component

---
+++ src/pages/post-auth/AdminAccount.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function AdminAccount(){
+
+    return(
+        <>
+            <div>
+                <h2>Admin account landing</h2>
+            </div>
+        </>
+    )
+}
