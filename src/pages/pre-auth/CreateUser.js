4c183dcb81a57c020bbfd135abb1816cf9acac5f
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-06-19 15:37:01 -0600

    added first name, last name, and placeholders for sign up form

--- src/pages/pre-auth/CreateUser.js
+++ src/pages/pre-auth/CreateUser.js
@@ -6,44 +6,51 @@ import { useState } from 'react'
 import Link from 'next/link'
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
-  const supabase = createClientComponentClient()
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+    const supabase = createClientComponentClient()
 
   const handleSignUp = async () => {
     await supabase.auth.signUp({
-      email,
-      password,
-      options: {
+        firstName,
+        lastName,
+        email,
+        password,
+        options: {
         emailRedirectTo: `${location.origin}/auth/callback`,
       },
     })
     router.refresh()
   }
 
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    router.refresh()
-  }
+//   const handleSignIn = async () => {
+//     await supabase.auth.signInWithPassword({
+//       email,
+//       password,
+//     })
+//     router.refresh()
+//   }
 
-  const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
+//   const handleSignOut = async () => {
+//     await supabase.auth.signOut()
+//     router.refresh()
+//   }
 
   return (
     <>
         <Link href="/">Home</Link>
-        <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input placeholder="First name" name="firstName" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+        <input placeholder="Last name" name="lastName" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+        <input placeholder="Email address" name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
         <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
+            placeholder="Password (minimum of 6 characters)"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
         />
         <button onClick={handleSignUp}>Sign up</button>
         {/* <button onClick={handleSignIn}>Sign in</button> */}
