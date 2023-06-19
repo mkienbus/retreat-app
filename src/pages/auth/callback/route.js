cff936a33f1ef29fe1233af2c926863c1a9ea920
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-06-19 15:24:59 -0600

    added Supabase auth for user login and creation - backend needs testing

--- src/pages/auth/callback/route.js
+++ src/pages/auth/callback/route.js
@@ -2,15 +2,15 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-// export async function GET(req) {
-  export default function GET(req) {
-  const supabase = createRouteHandlerClient({ cookies })
-  const { searchParams } = new URL(req.url)
-  const code = searchParams.get('code')
+export async function GET(request) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
 
   if (code) {
-    supabase.auth.exchangeCodeForSession(code)
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(new URL('http://localhost:3000/post-auth/UserAccount', req.url))
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
 }
--- src/pages/pre-auth/CreateUser.js
+++ src/pages/pre-auth/CreateUser.js
@@ -1,17 +1,53 @@
+'use client'
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import Link from 'next/link'
-// import CreateUserForm from './components/CreateUserForm'
-import AuthForm from '../components/AuthForm'
-import styles from '@/styles/createUser.module.css'
 
-export default function CreateUser(){
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+  const supabase = createClientComponentClient()
 
-    return(
-        <>
-            <Link href="/">Home</Link>
-            <div className={styles.createUserFormParent}>
-                <AuthForm />
-                {/* <CreateUserForm /> */}
-            </div>
-        </>
-    )
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    })
+    router.refresh()
+  }
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    router.refresh()
+  }
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+
+  return (
+    <>
+        <Link href="/">Home</Link>
+        <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        />
+        <button onClick={handleSignUp}>Sign up</button>
+        {/* <button onClick={handleSignIn}>Sign in</button> */}
+        {/* <button onClick={handleSignOut}>Sign out</button> */}
+    </>
+  )
 }
