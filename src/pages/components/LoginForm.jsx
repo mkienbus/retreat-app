8ae6e0bf9400e3ec27ac082d86b8f7781f292490
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-06-21 17:49:30 -0600

    matched text to gray

--- src/pages/components/LoginForm.jsx
+++ src/pages/components/LoginForm.jsx
@@ -24,6 +24,7 @@ export default function LoginForm(){
                     variables: {
                         default: {
                           colors: {
+                            inputLabelText: 'gray',
                             inputText: 'gray'
                           },
                         },
